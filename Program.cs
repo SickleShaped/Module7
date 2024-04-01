12d1503a5793ee26c71605d74827fbee400bc5f2
Филиппов Алексей <teenwizard.fornewvideo@gmail.com>
2024-04-01 19:31:52 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Module7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SmartHelper helper = new SmartHelper("Олег");
+            helper.Greetings("Грег");
+
+            Console.ReadKey();
+        }
+
+    }
+
+    class SmartHelper
+    {
+        private string name;
+
+        public SmartHelper(string name)
+        {
+            this.name = name;
+        }
+
+        public void Greetings(string name)
+        {
+            Console.WriteLine("Привет, {0}, я интеллектуальный помощник {1}", name, this.name);
+        }
+    }
+    
+}
